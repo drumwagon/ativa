0380c106629b703b0f2ad96d910113dcb605dd0a
Jeff Conner <drumwagon@gmail.com>
2014-01-08 14:18:37 -0600

    Fix errors in routes and project controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Ativa::Application.routes.draw do
   devise_for :users
   resources :posts
   root 'projects#index'
-  resources :project 
+  resources :projects 
     resources :subscriptions, only: [:create, :destroy]
   
-
   get 'users/:id' => 'users#show', as: 'user'
 end
 
